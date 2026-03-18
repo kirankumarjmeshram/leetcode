f353f6a1ebcc705fbd2dbf36ab906da6aef91010
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-03-18 16:17:39 +0530

    Time: 2 ms (96.04%) | Memory: 57.8 MB (52.75%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,14 +3,22 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
-    let n = nums.length;
-    let newArr = new Array(n).fill(0);
-    for(let i=0;i<n;i++){
-        for(let j=0;j<n;j++){
-            if((nums[i]> nums[j]) && i!==j){
-                newArr[i]++
-            }
+    // let n = nums.length;
+    // let newArr = new Array(n).fill(0);
+    // for(let i=0;i<n;i++){
+    //     for(let j=0;j<n;j++){
+    //         if((nums[i]> nums[j]) && i!==j){
+    //             newArr[i]++
+    //         }
+    //     }
+    // }
+    // return newArr;
+    let sortedNums = [...nums].sort((a,b) => a-b);
+    let map = {};
+    for(let i=0;i<sortedNums.length;i++) {
+        if(map[sortedNums[i]] === undefined){
+            map[sortedNums[i]] = i;
         }
     }
-    return newArr;
+    return nums.map(num=>map[num]);
 };
