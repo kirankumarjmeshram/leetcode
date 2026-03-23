d5239f8d4766f1ff1176b8605e8d0d96dd79f0e7
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-03-23 17:00:23 +0530

    Time: 2 ms (32.55%) | Memory: 58.2 MB (25.22%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.js
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.js
@@ -3,15 +3,14 @@
  * @return {number[]}
  */
 var finalPrices = function(prices) {
-    let n = prices.length;
-    let ans = [...prices];
-    for(let i=0;i<n;i++) {
-        for(let j=i+1;j<n;j++){
-            if(prices[j] <= prices[i] ){
-                ans[i] =prices[i] -  prices[j];
-                break;
-            }
+    let result = [...prices];
+    let stack = [];
+    for( let i=0;i<prices.length;i++){
+        while(stack.length>0 && prices[stack[stack.length-1]] >= prices[i]) {
+            let index = stack.pop();
+            result[index] = prices[index] - prices[i]
         }
+        stack.push(i);
     }
-    return ans;
+    return result;
 };
