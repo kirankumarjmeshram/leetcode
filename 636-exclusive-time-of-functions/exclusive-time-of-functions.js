b6c2ccd255c9ffa42cfccd4b4c3e53b244288947
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-03-23 16:09:07 +0530

    Time: 11 ms (82.13%) | Memory: 61.1 MB (45.38%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.js
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.js
@@ -6,9 +6,12 @@
 
 var exclusiveTime = function(n, logs) {
     const res = new Array(n).fill(0);
+    // [3,4]
     const stack = [];
+    // [0,]
     let prevTime = 0;
-
+    // 6
+// n = 2, logs = ["0:start:0","1:start:2","1:end:5","0:end:6"]
     for (const log of logs) {
         const [idStr, type, timeStr] = log.split(":");
         const id = Number(idStr);
