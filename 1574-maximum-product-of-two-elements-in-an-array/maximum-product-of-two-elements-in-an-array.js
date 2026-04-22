32b32a24423717d7b772b6c214996f2d03084d4a
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-04-22 10:21:14 +0530

    Time: 1 ms (76.73%) | Memory: 55.9 MB (22.04%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
@@ -5,21 +5,22 @@
 var maxProduct = function(nums) {
     let n = nums.length;
     let firstMax = 0, secondMax = 0;
-    // if(nums[0]>nums[1]){
-    //     firstMax = nums[0];
-    //     secondMax = nums[1]
-    // }else{
-    //     firstMax = nums[1];
-    //     secondMax = nums[0]     
+    // for (let i=0;i<n;i++) {
+    //     if(nums[i] > secondMax){
+    //         secondMax = nums[i]
+    //     }
+    //     if(secondMax> firstMax){
+    //         let temp = firstMax;
+    //         firstMax =secondMax;
+    //         secondMax = temp
+    //     }
     // }
-    for (let i=0;i<n;i++) {
-        if(nums[i] > secondMax){
-            secondMax = nums[i]
-        }
-        if(secondMax> firstMax){
-            let temp = firstMax;
-            firstMax =secondMax;
-            secondMax = temp
+    for(let num of nums) {
+        if(num > firstMax){
+            secondMax = firstMax;
+            firstMax = num;
+        }else if(num > secondMax){
+            secondMax = num;
         }
     }
     return (firstMax-1)*(secondMax-1);
