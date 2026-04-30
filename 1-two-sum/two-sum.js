86962b0be1b7dd11f4e6c38e9fe3661f81ba42fd
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-04-30 22:34:00 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (61.51%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,11 +5,20 @@
  */
 var twoSum = function(nums, target) {
     let n=nums.length;
-        for(let i=0;i<n;i++){
-            for(let j=i+1;j<n;j++){
-                if((nums[i]+nums[j])===target){
-                    return [i,j]
-                }
-            }
+    // for(let i=0;i<n;i++){
+    //     for(let j=i+1;j<n;j++){
+    //         if((nums[i]+nums[j])===target){
+    //             return [i,j]
+    //         }
+    //     }
+    // }
+    let map = {};
+    for (let i=0;i<n;i++) {
+        let num = nums[i];
+        let complement = target-num
+        if(complement in map){
+            return [i, map[complement]]
         }
+        map[num] = i
+    }
 };
