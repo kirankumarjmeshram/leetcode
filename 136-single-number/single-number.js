87dcad6dfda684bc5a8fe74b933df29efb5b83dc
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-04-30 08:00:56 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (74.76%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let ans = nums[0]
-    for(let i=1;i<nums.length;i++) {
+    // let ans = nums[0]
+    let ans = 0;
+    for(let i=0;i<nums.length;i++) {
         ans^=nums[i]
     }
     return ans;
