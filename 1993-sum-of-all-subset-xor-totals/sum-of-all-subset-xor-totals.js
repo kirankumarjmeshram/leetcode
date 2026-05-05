01e6c494cff9b941afe6bddc9ac85409800abda8
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-05 22:11:14 +0530

    Time: 4 ms (52.40%) | Memory: 52.7 MB (95.94%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subsetXORSum = function(nums) {
+    let n = nums.length;
+    let sum = 0;
+    for(let i=0;i<1<<n;i++){
+        let sub = 0;
+        for(j=0;j<n;j++){
+            
+            if(i&(1<<j)){
+                sub^=nums[j]
+            }
+            
+        }
+        sum+=sub;
+    }
+    return sum;
+};
