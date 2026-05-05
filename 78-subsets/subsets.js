fba87bc86515dffdef4b146c131bb7191e860d8d
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-05 16:35:49 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (58.88%) - LeetSync

--- 78-subsets/subsets.js
+++ 78-subsets/subsets.js
@@ -4,14 +4,26 @@
  */
 var subsets = function(nums) {
     let n = nums.length;
-    let subSets = [[]];
-    for (let num of nums) {
-        let length = subSets.length;
-        for(let j = 0;j<length;j++){
-            let subSet = subSets[j];
-            subSets.push([...subSet, num])
-            // subSets.push(subSet.concat(num))
+    // let subSets = [[]];
+    // for (let num of nums) {
+    //     let length = subSets.length;
+    //     for(let j = 0;j<length;j++){
+    //         let subSet = subSets[j];
+    //         subSets.push([...subSet, num])
+    //         // subSets.push(subSet.concat(num))
+    //     }
+    // }
+    // using bit masking
+    let subSets = []
+    for(let i=0;i< 1<<n;i++){
+        let sub = [];
+        for(let j = 0; j<n;j++) {
+            if(i & (1<<j)){
+                sub.push(nums[j])
+            }
         }
+        subSets.push(sub)
     }
+
     return subSets;
 };
