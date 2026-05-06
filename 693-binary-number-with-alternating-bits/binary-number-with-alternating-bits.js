f0b4eeaac33545d02d9fcb4bc5c8c4565bbc9f28
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-06 11:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (72.48%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.js
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.js
@@ -3,14 +3,16 @@
  * @return {boolean}
  */
 var hasAlternatingBits = function(n) {
-    let prev = n&1
-    while(n) {
-        n>>=1
-        let curr = n&1;
-        if(prev === curr){
-            return false
-        }
-        prev = curr
-    }
-    return true;
+    // let prev = n&1
+    // while(n) {
+    //     n>>=1
+    //     let curr = n&1;
+    //     if(prev === curr){
+    //         return false
+    //     }
+    //     prev = curr
+    // }
+    // return true;
+    let x = n^(n>>1);
+    return !(x&(x+1));
 };
