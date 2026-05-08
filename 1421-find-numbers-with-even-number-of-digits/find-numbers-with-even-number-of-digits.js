3550f3e6cfbca3f4963d0c0364f175ecd95fd9fb
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-08 08:05:35 +0530

    Time: 1 ms (82.82%) | Memory: 55.5 MB (47.89%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -5,18 +5,24 @@
 var findNumbers = function(nums) {
     count = 0
     for(let num of nums) {
-       if(countDigit(num)%2 === 0){
-        count++;
-       }
+    //    if(countDigit(num)%2 === 0){
+        if((num>9) && (num <=99) || 
+            (num>999) && (num <=9999) ||
+            (num>99999) && (num <=999999) 
+            ){
+                 count++;
+            }
+       
+       
     }
     return count;
 };
 
-function countDigit(num){
-    let count = 0;
-    while(num>0){
-        num = Math.floor(num/10);
-        count++;
-    }
-    return count;
-}
+// function countDigit(num){
+//     let count = 0;
+//     while(num>0){
+//         num = Math.floor(num/10);
+//         count++;
+//     }
+//     return count;
+// }
