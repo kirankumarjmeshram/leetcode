5712c0b290d9f82a5b1bda2a20a1715b7cd8211f
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-08 08:10:08 +0530

    Time: 38 ms (87.84%) | Memory: 53.8 MB (39.64%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.js
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.js
@@ -4,4 +4,15 @@
  */
 var numberOfMatches = function(n) {
     return n-1
+    // let matches = 0;
+    // while(n>1){
+    //     if(n%2 ===0){
+    //         matches += n/2;
+    //         n = n/2;
+    //     }else{
+    //         matches += (n - 1) / 2
+    //         n = (n +1) / 2 
+    //     }
+    // }
+    // return matches
 };
