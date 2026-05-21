0d62a6751f497643231eba22468577af1c2f71ff
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-21 08:19:36 +0530

    Time: 4 ms (15.17%) | Memory: 55.9 MB (22.28%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
@@ -3,12 +3,21 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    let letters =[ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    for(let letter of letters){
-        if(!sentence.includes(letter)){
-            return false;
+    // let letters =[ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    // for(let letter of letters){
+    //     if(!sentence.includes(letter)){
+    //         return false;
+    //     }
+    // }
+    // return true;
+    let freqArr = new Array(26).fill(false);
+    let distinctChar = 0;
+    for(let s of sentence ){
+        if(freqArr[s.charCodeAt(0)-97]===false){
+            freqArr[s.charCodeAt(0)-97]=true;
+            distinctChar++;
         }
     }
-    return true;
+    return distinctChar===26
 
 };
