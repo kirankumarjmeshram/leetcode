77d0b9246dbc2e4fdd60bb07638133c5885816dd
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-22 15:20:13 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (5.20%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let count=0;
+    let n = nums.length;
+    for(let i=0;i<n;i++){
+        for(let j=i+1;j<n;j++) {
+            if((nums[i] == nums[j])&&(i < j)) count++;
+        }
+    }
+    return count;
+};
