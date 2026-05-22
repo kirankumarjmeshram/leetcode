a2b17ce1e297dd7dbda699a7defe6f26ce429d9b
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-22 15:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (37.11%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.js
+++ 728-self-dividing-numbers/self-dividing-numbers.js
@@ -14,18 +14,20 @@ var selfDividingNumbers = function(left, right) {
 };
 
 function checkSelfDividingNumbers(num){
-    let nums = [];
+    // let nums = [];
     let temp = num;
     while(temp>0){
-        nums.push(temp%10);
+        // nums.push(temp%10);
+        let digit = temp%10
+        if(digit===0 || num%digit!==0) return false;
         temp = Math.floor(temp/10)
     }
-    for(let n of nums){
-        if(num%n===0){
-            continue
-        } else{
-            return false
-        }
-    }
+    // for(let n of nums){
+    //     if(num%n===0){
+    //         continue
+    //     } else{
+    //         return false
+    //     }
+    // }
     return true;
 }
