054243b7a7cda8f3e3a436e8e99e71f64f8628f7
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-22 15:09:46 +0530

    Time: 9 ms (12.89%) | Memory: 57.3 MB (20.31%) - LeetSync

---
+++ 728-self-dividing-numbers/self-dividing-numbers.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    let ans =[];
+    for(let i=left; i<=right;i++){
+        if(checkSelfDividingNumbers(i)){
+            ans.push(i);
+        }
+    }
+    return ans;
+};
+
+function checkSelfDividingNumbers(num){
+    let nums = [];
+    let temp = num;
+    while(temp>0){
+        nums.push(temp%10);
+        temp = Math.floor(temp/10)
+    }
+    for(let n of nums){
+        if(num%n===0){
+            continue
+        } else{
+            return false
+        }
+    }
+    return true;
+}
