1ffdf79e9d3313b7e20f0cdeb2ead8fb59d95a26
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-23 22:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (60.69%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    let fault = 0;
+    // as array is rotary there will be only one case where nums[i-1]>nums[i] 
+    //as array is non decreasing order else other will be nums[i-1] < nums[i]
+    let n = nums.length;
+    for(let i=0;i<n;i++) {
+        if(nums[i]>nums[(i+1)%n]){
+            if(fault) return false;
+            fault += 1;
+        }
+        
+    }
+    return true;
+};
