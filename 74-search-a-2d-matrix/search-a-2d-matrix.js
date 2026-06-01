b9a3552eccc23379c095f1be51f1640f4071f950
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-06-01 15:36:14 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (46.72%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let row = 0;
+    let col = matrix[0].length-1;
+    while(row<matrix.length && col >=0){
+        if(matrix[row][col] > target){
+            col--;
+        }if(matrix[row][col]<target){
+            row++
+        }else if(matrix[row][col]===target){
+            return true;
+        }
+    }
+        return false;
+};
