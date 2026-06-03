4f81a512dd22e031d3f2c672a8ba7ee9ee9346ea
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-06-03 17:15:40 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (89.10%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    let n = nums.length;
+    let l = 0;
+    let h = n-1;
+    while(l<=h) {
+        let m = Math.floor(l+(h-l)/2);
+        if(nums[m]===target){
+            return true;
+        }if(nums[l]===nums[m]){
+            l++;
+            continue;
+        }else if(nums[l]<=nums[m]){
+            if((target<nums[m])&& target>=nums[l]){
+                 h= m-1;
+                 // Target lies in the sorted left half
+            }else{
+                l = m+1
+                // Target is not in left half, search right half
+            }
+           
+        }else{
+            if((target > nums[m]) && (target <= nums[h])){
+                l = m+1
+                // Target lies in the sorted right half
+            }else{
+                h=m-1
+                // Target is not in right half, search left half
+            }
+            
+        }
+    }
+    return false;
+};
