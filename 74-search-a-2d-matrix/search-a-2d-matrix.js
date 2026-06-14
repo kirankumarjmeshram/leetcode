95e9bfab880e5f69d3ae1757cbdd1d3749ef4013
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-06-14 09:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 52.6 MB (96.61%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,15 +4,34 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let row = 0;
-    let col = matrix[0].length - 1;
-    while(row<matrix.length && col>=0){
-        if(target === matrix[row][col]){
+    // let row = 0;
+    // let col = matrix[0].length - 1;
+    // while(row<matrix.length && col>=0){
+    //     if(target === matrix[row][col]){
+    //         return true;
+    //     }if(target < matrix[row][col]){
+    //         col--;
+    //     }else{
+    //         row++
+    //     }
+    // }
+    // return false;
+
+    let rows = matrix.length;;
+    let cols = matrix[0].length;
+    let left = 0;
+    let right = rows * cols-1;
+    while(left<=right){
+        let mid = left + Math.floor((right-left)/2);
+        let row = Math.floor(mid/cols);
+        let col = mid%cols
+        let value = matrix[row][col];
+        if(target === value){
             return true;
-        }if(target < matrix[row][col]){
-            col--;
+        }if(target < value){
+            right = mid-1;
         }else{
-            row++
+            left = mid+1
         }
     }
     return false;
