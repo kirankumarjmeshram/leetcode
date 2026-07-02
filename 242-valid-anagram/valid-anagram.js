63c2b48e9b84a4ab444cf34dea4b2a115946811b
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-02 07:54:10 +0530

    Time: 15 ms (51.17%) | Memory: 55.6 MB (76.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,32 +3,23 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-        let n = s.length;
-        if(n !== t.length) return false;
-        let mapS = {};
-        let mapT = {};
-        for(let i=0;i<n;i++){
-            if(!mapS[s[i]]){
-                mapS[s[i]] = 1;
-            }else{
-                mapS[s[i]]++;
-            }
-        }
-
-        for(let i=0;i<n;i++){
-            if(!mapT[t[i]]){
-                mapT[t[i]] = 1;
-            }else{
-                mapT[t[i]]++;
-            }
-        }
-
-        for(let el of Object.keys(mapS)){
-            if(mapS[el] !== mapT[el]){
-                return false
-            }
-        }
-        return true;
-
+var isAnagram = function (s, t) {
+    let n = s.length;
+    if (n !== t.length) return false;
+    let mapS = {};
+    let mapT = {};
+    for (let i = 0; i < n; i++) {
+        mapS[s[i]] = (mapS[s[i]] || 0) +1
     }
+
+    for (let i = 0; i < n; i++) {
+        mapT[t[i]] = (mapT[t[i]] || 0) +1
+    }
+
+    for (let el of Object.keys(mapS)) {
+        if (mapS[el] !== mapT[el]) {
+            return false;
+        }
+    }
+    return true;
+};
