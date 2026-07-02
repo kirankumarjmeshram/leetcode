cb6388add993aefe09793e4f555e3e9655c4db59
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-02 08:06:49 +0530

    Time: 9 ms (86.48%) | Memory: 57.7 MB (49.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -6,20 +6,15 @@
 var isAnagram = function (s, t) {
     let n = s.length;
     if (n !== t.length) return false;
-    let mapS = {};
-    let mapT = {};
-    for (let i = 0; i < n; i++) {
-        mapS[s[i]] = (mapS[s[i]] || 0) +1
+    let map = {};
+    for (let el of s) {
+        map[el] = (map[el] || 0) +1
     }
 
-    for (let i = 0; i < n; i++) {
-        mapT[t[i]] = (mapT[t[i]] || 0) +1
+    for (let el of t) {
+        if(!map[el]) return false;
+        map[el]--;
     }
 
-    for (let el of Object.keys(mapS)) {
-        if (mapS[el] !== mapT[el]) {
-            return false;
-        }
-    }
     return true;
 };
