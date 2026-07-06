92afbeaec27c39d920bcc34dd41995b04fcdf45d
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-06 17:34:54 +0530

    Time: 2 ms (71.16%) | Memory: 64 MB (49.48%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -3,13 +3,18 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let maxSum = nums[0];
-    let currSum = nums[0]
     let n = nums.length;
+    let maxSum = nums[0];
+    let sum = 0;
 
-    for(let i=1; i<n;i++) {
-        currSum = Math.max(nums[i],currSum+nums[i]);
-        maxSum = Math.max(currSum, maxSum);
+    for(let i=0;i<n;i++) {
+        sum += nums[i];
+        if(sum > maxSum){
+            maxSum = sum
+        }
+        if(sum < 0) {
+            sum = 0;
+        }
     }
     return maxSum;
 };
