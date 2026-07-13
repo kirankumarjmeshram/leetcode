5db5fac219e3c4c7761c126458195ddd9a3389e1
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-13 08:30:26 +0530

    Time: 2 ms (94.91%) | Memory: 58.8 MB (35.80%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -4,22 +4,19 @@
  */
 var evalRPN = function(tokens) {
     let stack = [];
-    for(let t of tokens){
-        if(t === '+') {
-            stack.push(stack.pop()+stack.pop())
-        }else if(t === '-') {
-            let second = stack.pop();
-            let first = stack.pop();
-            stack.push(first - second);
-        }else if(t === '*'){
-            stack.push(stack.pop()*stack.pop())
-        }else if(t === "/") {
-            let second = stack.pop();
-            let first = stack.pop();
-            stack.push(parseInt(first/second));
+
+    for(let token of tokens) {
+        if(isNaN(token)) {
+            let a = stack.pop();
+            let b = stack.pop();
+
+            if(token === '+') stack.push(a+b);
+            if(token === '-') stack.push(b-a);
+            if(token === '*') stack.push(a*b);
+            if(token === "/") stack.push(Math.trunc(b/a));
         }else{
-            stack.push(parseInt(t))
+            stack.push(Number(token));
         }
     }
-    return stack[0]
+    return stack.pop();
 };
