77b2ac2f5222b75b078474619518cec6fb633f3f
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-27 08:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 75.1 MB (13.15%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.js
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.js
@@ -11,22 +11,48 @@
  * @return {number}
  */
 var countNodes = function(root) {
+        function leftHeight(node) {
+        let h = 0;
+        while (node) {
+            h++;
+            node = node.left;
+        }
+        return h;
+    }
+
+    function rightHeight(node) {
+        let h = 0;
+        while (node) {
+            h++;
+            node = node.right;
+        }
+        return h;
+    }
+
+    if (root === null) return 0;
+    let lh = leftHeight(root);
+    let rh = rightHeight(root);
+
+    if (lh === rh) {
+        return Math.pow(2, lh) - 1;
+    }
+    return 1 + countNodes(root.left) + countNodes(root.right);
     // if(root===null){
     //     return 0;
     // }
     // return 1 + countNodes(root.left) + countNodes(root.right);
-    if (root === null) return 0;
+    // if (root === null) return 0;
 
-    let queue = [root];
-    let count = 0;
+    // let queue = [root];
+    // let count = 0;
 
-    while (queue.length) {
-        let node = queue.shift();
-        count++;
+    // while (queue.length) {
+    //     let node = queue.shift();
+    //     count++;
 
-        if (node.left) queue.push(node.left);
-        if (node.right) queue.push(node.right);
-    }
+    //     if (node.left) queue.push(node.left);
+    //     if (node.right) queue.push(node.right);
+    // }
 
-    return count;
+    // return count;
 };
