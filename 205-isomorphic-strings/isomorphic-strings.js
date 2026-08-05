63a4fb7b8f05175aae545e7a6e7dba157777d302
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-08-05 23:00:02 +0530

    Time: 8 ms (35.03%) | Memory: 55.1 MB (49.49%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let sMap = {};
+    let tMap = {};
+    let n = s.length;
+    let m = t.length;
+
+    if(n !== m) return false;
+
+    for(let i=0;i<n;i++) {
+        let sChar = s[i];
+        let tChar = t[i];
+        if(sMap[sChar] && sMap[sChar] !== tChar || tMap[tChar] && tMap[tChar] !== sChar) {
+            return false;
+        }
+        sMap[sChar] = tChar;
+        tMap[tChar] = sChar;
+        
+    }
+   return true;
+};
