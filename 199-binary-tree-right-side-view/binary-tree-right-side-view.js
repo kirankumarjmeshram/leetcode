00292069feb5bce8254825208fa209f37752e9cd
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-08-05 07:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (53.06%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if (!root) return []
+    let ans = [];
+    let queue = [root];
+    while(queue.length > 0) {
+        let n = queue.length;
+        for(let i=0; i< n; i++) {
+            let curr = queue.shift();
+            if(i=== n-1) ans.push(curr.val);
+            if(curr.left) queue.push(curr.left);
+            if(curr.right) queue.push(curr.right);
+        }
+    }
+    return ans;
+};
