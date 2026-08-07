46f5b6271c4d6763a4d0c6527c9c40027e3473a3
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-08-07 16:34:34 +0530

    Time: 8 ms (37.84%) | Memory: 64.5 MB (75.68%) - LeetSync

---
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var smallestFromLeaf = function(root) {
+    let smallestString = "";
+    dfs(root, []);
+    return smallestString;
+
+    function dfs(root, path) {
+        if(root === null) return;
+
+        path.push(String.fromCharCode(root.val + 97));
+
+        if(root.left === null && root.right === null) {
+            let str = [...path].reverse().join("");
+            if(smallestString === "" || str < smallestString) {
+                smallestString = str;
+            }
+        }
+
+        dfs(root.left, path);
+        dfs(root.right, path);
+        path.pop();
+    }
+};
