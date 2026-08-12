def78f845841bd6d3fc633bf48a6b972d7007e63
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-08-12 07:46:12 +0530

    Time: 1 ms (59.85%) | Memory: 52.7 MB (94.03%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -5,10 +5,16 @@
  */
 var uniquePaths = function(m, n) {
     let dp = Array.from({length:m},()=>new Array(n).fill(1));
-    for(let i=1;i<m;i++){
-        for(let j=1;j<n;j++) {
-            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+    // for(let i=1;i<m;i++){
+    //     for(let j=1;j<n;j++) {
+    //         dp[i][j] = dp[i-1][j] + dp[i][j-1];
+    //     }
+    // }
+    // return dp[m-1][n-1];
+    for(let i=m-2;i>=0;i--){
+        for(let j=n-2;j>=0;j--) {
+            dp[i][j] = dp[i+1][j] + dp[i][j+1];
         }
     }
-    return dp[m-1][n-1];
+    return dp[0][0];
 };
