48beb8152bcdbc51c8cc7eb31293bf605dcad51c
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-08-13 07:54:40 +0530

    Time: 1 ms (53.63%) | Memory: 52.8 MB (95.16%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+    let dp = Array.from({length:m},()=>new Array(n).fill(0));
+    let flag = false
+    for(let i=0;i<m;i++) {
+        if(flag || obstacleGrid[i][0]){
+            flag = true
+            dp[i][0] = 0;
+        }else{
+            dp[i][0] = 1;
+        }
+    }
+    flag = false;
+    for(let j=0;j<n;j++) {
+        if(flag || obstacleGrid[0][j]){
+            flag = true
+            dp[0][j] = 0;
+        }else{
+            dp[0][j] = 1;
+        }
+    }
+    for(let i=1;i<m;i++){
+        for(let j=1;j<n;j++) {
+            if(obstacleGrid[i][j] === 1){
+                dp[i][j] = 0;
+            }else{
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+            
+        }
+    }
+    return dp[m-1][n-1];
+};
