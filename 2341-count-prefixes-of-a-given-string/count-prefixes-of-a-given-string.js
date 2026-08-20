d2c2937ee46543f793e12ea4a927cf5bf965d64b
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-08-20 18:26:26 +0530

    Time: 16 ms (1.22%) | Memory: 61.1 MB (6.10%) - LeetSync

--- 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.js
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.js
@@ -4,11 +4,46 @@
  * @return {number}
  */
 var countPrefixes = function(words, s) {
-    let count = 0;
-    for(let word of words) {
-        if(s.startsWith(word)){
-            count++;
-        }
+    let trie = new Trie();
+    for(let word of words){
+        trie.insert(word);
     }
-    return count;
+    return trie.countPrefixes(s);
 };
+
+class TrieNode{
+    constructor() {
+        this.children = {};
+        this.count = 0;
+    }
+}
+
+class Trie{
+    constructor(){
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let node = this.root;
+        for(let char of word) {
+            if(!node.children[char]) {
+                node.children[char] = new TrieNode()
+            }
+            node = node.children[char];
+        }
+        node.count++;
+    }
+
+    countPrefixes(s) {
+        let node = this.root
+        let count = 0;
+        for(let char of s) {
+            if(!node.children[char]) {
+                break
+            }
+            node =  node.children[char];
+            count += node.count
+        }
+        return count;
+    }
+}
