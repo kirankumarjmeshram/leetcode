9ce5551e68f1cd31e942623a698f3e8a905582c5
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-08-20 19:26:22 +0530

    Time: 7 ms (10.42%) | Memory: 55.1 MB (30.65%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,16 +3,44 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let first = strs[0];
-
-    for(let i=0;i<first.length;i++) {
-        let char = first[i];
-
-        for(let j=1; j< strs.length; j++) {
-            if(strs[j][i] !== char){
-                return first.substring(0,i);
-            }
-        }
+    let trie = new Trie();
+    for(let str of strs) {
+        trie.insert(str);
     }
-    return first;
+    return trie.longestCommonPrefix();
 };
+
+class TrieNode{
+    constructor() {
+        this.children = {};
+        this.isEnd = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let node = this.root;
+        for(let char of word) {
+            if(!node.children[char]){
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+        node.isEnd = true;
+    }
+    longestCommonPrefix(){
+        let node = this.root;
+        let prefix = "";
+
+        while(Object.keys(node.children).length === 1 && !node.isEnd) {
+            let char = Object.keys(node.children)[0];
+            prefix += char;
+            node = node.children[char]
+        } 
+        return prefix;
+    }
+}
