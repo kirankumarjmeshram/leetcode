630b5c5e6feb17c927eb46d8a916151d69b8c0f1
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-09-02 17:00:53 +0530

    Time: 1 ms (86.18%) | Memory: 55.7 MB (71.46%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -6,13 +6,14 @@
 var canPlaceFlowers = function(flowerbed, n) {
     let len = flowerbed.length;
     let count = 0
-    if(n===0) return true;
+    // if(n===0) return true;
     for(let i=0;i<len;i++) {
         if(flowerbed[i] === 0 && (i===0 || flowerbed[i-1] === 0) && (i=== len -1 || flowerbed[i+1] === 0)){
             flowerbed[i] = 1;
-            count++;
-            if(count === n) return true;
+            // count++;
+            // if(count === n) return true;
+            n--;
         }
     }
-    return false;
+    return n<=0;
 };
