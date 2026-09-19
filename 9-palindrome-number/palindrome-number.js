8ba233bf8320f0cec014a0b7c0a44f46ab16e98a
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-09-19 21:04:09 +0530

    Time: 65 ms (5.07%) | Memory: 64.7 MB (36.60%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -2,20 +2,17 @@
  * @param {number} x
  * @return {boolean}
  */
-
 var isPalindrome = function(x) {
+    let num = x
+    let palNum = 0;
     if(x<0) return false;
-    let arr =[];
-    while(x>0)
-    {
-        arr.push(x%10);
-        x =Math.floor(x/10);
+
+    while(x>0){
+        palNum*=10;
+        palNum += x%10;
+        
+        x = Math.floor(x/10)
     }
-    let n = arr.length;
-    for(let i=0;i<Math.floor(n/2);i++){
-        if(arr[i]!==arr[n-i-1]){
-            return false;
-        }
-    }
-    return true;
+    console.log(palNum)
+    return num===palNum;
 };
